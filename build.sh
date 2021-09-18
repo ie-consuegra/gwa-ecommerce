58389b11d04b99c032bfd39d5ff568c1108c12b0
Ivan Consuegra <ie.consuegra@gmail.com>
2021-09-18 15:03:14 -0500

    Batch enhancements

--- build.sh
+++ build.sh
@@ -5,18 +5,18 @@ rm src/client/bundles/bundle.js
 rm src/client/bundles/bundle.css
 echo "✔️  Done"
 printf "\n"
-echo "*  Bundling backend JavaScript..."
+echo "*  Bundling backend JavaScript files..."
 ls -1 src/server/*.js | sort | while read fn ; do cat "$fn" >> src/server/bundle/bundle.js; done
 echo "✔️  Done, file can be found as bundle.js in src/server/ directory"
 printf "\n"
-echo "*  Bundling frontend files..."
-echo "*  JavaScript..."
+echo "*  Bundling frontend..."
+echo "*  JavaScript files..."
 ls -1 src/client/js/*.js | sort | while read fn ; do cat "$fn" >> src/client/bundles/bundle.js; done
 echo "✔️  Done, file can be found as bundle.js in src/client/bundles"
-echo "*  CSS..."
+echo "*  CSS files..."
 ls -1 src/client/css/*.css | sort | while read fn ; do cat "$fn" >> src/client/bundles/bundle.css; done
-echo "✔️  Done, file can be found as bundle.js in src/client/bundles"
-echo "*  Building frontend.html file..."
+echo "✔️  Done, file can be found as bundle.css in src/client/bundles"
+echo "*  HTML, CSS and JS..."
 npx html-build -c html-build-config.js src/client/index.html src/client/bundles/bundle.html
 echo "✔️  Done, file can be found as bundle.html in src/client/bundles"
 printf "\n"
