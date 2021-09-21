e434759dd009bb1e3c180e719f2c31dd4101b85a
Ivan Consuegra <ie.consuegra@gmail.com>
2021-09-21 10:44:47 -0500

    Add fetch data from database
    - Use of SheetsDB for fetching data from spreadsheet
    - New method fetchStockOnInit()
    - Add the inCart attribute when setting app data

--- src/client/js/c-appXData.js
+++ src/client/js/c-appXData.js
@@ -7,7 +7,7 @@
 function appXData() {
   return {
     // Store stuff
-    data,
+    data: [],
     formatCurrency,
     isAvailable,
     isActive,
@@ -20,5 +20,19 @@ function appXData() {
 
     // Cart stuff
     ...cart,
+
+    // Server calls
+    fetchStockOnInit() {
+      const self = this;
+      google
+        .script
+        .run
+        .withSuccessHandler((stock) => {
+          // Add inCart attribute to each stock item
+          const stockData = JSON.parse(stock).map((item) => ({ ...item, inCart: 0 }));
+          self.data = stockData;
+        })
+        .queryStock();
+    },
   };
 }
---
+++ src/server/b-main.js
@@ -0,0 +1,26 @@
+function queryStock() {
+  const url = 'https://docs.google.com/spreadsheets/d/1zME7BfSEP1CtYHNklxXWAVtcf4n5XH1lhjRxLoKK470/edit';
+
+  const types = {
+    1: {
+      code: 'S',
+      name: 'S',
+      unit: 'S',
+      type: 'S',
+      category: 'S',
+      cost: 'N',
+      price: 'N',
+      minimumStock: 'S',
+      stock: 'S',
+      imgUrl: 'S',
+    },
+  };
+
+  const connection = SheetsDB.connect(url, types);
+
+  const refs = connection.getSheetRefs();
+  Logger.log(`\nList of refs:\n ${JSON.stringify(refs, null, ' ')}`);
+
+  const data = connection.table('db', types).get();
+  return JSON.stringify(data, null, ' ');
+}
