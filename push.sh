8edaee30009126f82b34a20e9d0951f54c0f050c
Ivan Consuegra <ie.consuegra@gmail.com>
2021-09-21 17:44:46 -0500

    Add loading spinner
    - Extra: Add -f flag to clasp push command to avoid warnings

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ sh ./build.sh
 cat ./src/client/bundles/bundle.html > ./dist/frontend.html
 cat ./src/server/bundle/bundle.js > ./dist/backend.js
 echo "🔼  Uploading..."
-clasp push
+clasp push -f
 echo "🚀  Done! Deploy finalized at: $(date +"%T")"
