8e8bc137b6639f1698e0844ccca76c9a0f16f8e5
Ivan Consuegra <ie.consuegra@gmail.com>
2021-09-25 12:45:52 -0500

    Add embed web app code

--- src/server/a-App.js
+++ src/server/a-App.js
@@ -1,6 +1,9 @@
 const App = {
   name: '',
   appSettings: {},
+  properties: {},
+  meta: {},
+  url: '',
 
   get settings() {
     return this.appSettings;
@@ -12,5 +15,42 @@ const App = {
 
   get isSetup() {
     return Object.keys(this.appSettings).length > 0;
+  },
+
+  fetchProperties() {
+    const properties = PropertiesService
+      .getScriptProperties()
+      .getProperties();
+
+    if (Object.keys(properties).length > 0) {
+      this.properties = properties;
+    } else {
+      this.properties = {};
+    }
+  },
+
+  getAppUrl() {
+    this.url = ScriptApp
+      .getService()
+      .getUrl();
+  },
+
+  getSettings() {
+    if (this.isSetup) {
+      this.settings = JSON.parse(this.properties.settings);
+    }
+  },
+
+  init() {
+    this.fetchProperties();
+    this.getSettings();
+    this.getAppUrl();
+
+    const { appSettings, url } = this;
+
+    this.meta = {
+      appSettings,
+      url,
+    };
   },
 };
--- src/server/b-main.js
+++ src/server/b-main.js
@@ -1,3 +1,5 @@
+App.init();
+
 function queryStock() {
   const url = 'https://docs.google.com/spreadsheets/d/1zME7BfSEP1CtYHNklxXWAVtcf4n5XH1lhjRxLoKK470/edit';
 
@@ -23,4 +25,8 @@ function queryStock() {
 
   const data = connection.table('db', types).get();
   return JSON.stringify(data, null, ' ');
+}
+
+function fetchAppMeta() {
+  return App.meta;
 }
